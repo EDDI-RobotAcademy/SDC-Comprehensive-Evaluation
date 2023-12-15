1f0e9456d4f983a610221b8669d63d694e394648
youngchan4 <149447336+youngchan4@users.noreply.github.com>
2023-12-15 17:23:20 +0900

    feat:[황영찬] 복합문제 24~

--- 1기/first/youngchanhwang/comp24/entity/Product.h
+++ 1기/first/youngchanhwang/comp24/entity/Product.h
@@ -5,12 +5,16 @@
 #ifndef TEST_APP_PRODUCT_H
 #define TEST_APP_PRODUCT_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
 extern unsigned int product_unique_id;
 
 typedef struct _product product;
 
-struct _product
-{
+struct _product {
     unsigned int id;
     unsigned int price;
     char *name;
@@ -18,5 +22,11 @@ struct _product
 };
 
 product *init_product_object(unsigned int price, char *name, char *detail);
+
+#ifdef __cplusplus
+}
+#endif
+
+
 
 #endif //TEST_APP_PRODUCT_H
