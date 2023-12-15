7dc5694314e9715f88d1d83510fc2c10e359425c
Leejaelim <149447215+Leejaelim@users.noreply.github.com>
2023-12-15 17:44:26 +0900

    feat:[이재림] comp24 답안 제출[SDC-Comprehensive-Evaluation]

--- 1기/first/jaelimlee/comp24/product/entity/Product.c
+++ 1기/first/jaelimlee/comp24/product/entity/Product.c
@@ -6,7 +6,8 @@ product_struct *createProduct(int id, double price, const char* name, const char
 
     tmp_struct->id = id;
     tmp_struct->price = price;
-
+    tmp_struct->name = *name;
+    tmp_struct->description = *description;
 
     return tmp_struct;
 }
