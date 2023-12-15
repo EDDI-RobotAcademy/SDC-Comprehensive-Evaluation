aa4a658ead9b389b874e6e8b0a7932b165421359
LoHenP <qwpoal119@naver.com>
2023-12-15 12:19:09 +0900

    feat:[이재승] 난수생성기 제작 [comp-x]

---
+++ 1기/first/jaeseunglee/comp12/utility/RandomGenerator.h
@@ -0,0 +1,18 @@
+//
+// Created by eddi on 23. 12. 15.
+//
+
+#ifndef COMP12_RANDOMGENERATOR_H
+#define COMP12_RANDOMGENERATOR_H
+
+#define RANDOM_MIN 0
+#define RANDOM_MAX 999
+
+class RandomGenerator {
+public:
+    static double generate();
+    static int generate(int min, int max);
+};
+
+
+#endif //COMP12_RANDOMGENERATOR_H
