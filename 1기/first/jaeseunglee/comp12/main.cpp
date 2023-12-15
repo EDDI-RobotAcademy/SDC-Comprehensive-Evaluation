c01b484169e11d7e80a059ebeea0a3ddc86cabf4
LoHenP <qwpoal119@naver.com>
2023-12-15 12:50:16 +0900

    feat: [이재승] 싱글톤 초기화 [comp14]

--- 1기/first/jaeseunglee/comp12/main.cpp
+++ 1기/first/jaeseunglee/comp12/main.cpp
@@ -1,11 +1,32 @@
 #include <iostream>
 #include "utility/random/RandomGenerator.h"
 #include "utility/sleep/Sleeper.h"
+#include "entity/Vector.h"
+#include "mysql/DbProcess.h"
+
+void initSingleton()
+{
+    const char* host = "localhost";
+    const char* user = "eddi";
+    const char* pass = "eddi@123";
+    const char* dbName = "comp_db";
+
+    DbProcess *dbInstance = DbProcess::getInstance(host, user, pass, dbName);
+    dbInstance->connect();
+}
+
+
+
 
 
 int main()
 {
-    int randomNumber = RandomGenerator::generate(300,1000);
-    Sleeper::sleep(randomNumber);
+
+    for(int i = 0; i<10; i++)
+    {
+        int randomNumber = RandomGenerator::generate(300,1000);
+        Sleeper::sleep(randomNumber);
+    }
+
     return 0;
 }
